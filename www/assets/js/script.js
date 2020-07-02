72da9af4f5ead9840e4df54dc859ec2341734b5b
Dicky Budianto <dickybudianto@usahakreatif.co.id>
2020-07-02 16:09:18 +0700

    update url newest

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,4 +1,4 @@
-﻿var urlService = 'https://169400dbb26d.ngrok.io/ronaldSengkey/fitClub/api/v1';
+﻿var urlService = 'https://c52e81e2ee30.ngrok.io/ronaldSengkey/fitClub/api/v1';
 // var urlService = 'http://localhost:8888/ronaldSengkey/fitClub/api/v1';
 var fieldTextInput = '<input type="text" class="form-control fieldText">';
 var fieldEmailInput = '<input type="email" class="form-control fieldEmail">';
