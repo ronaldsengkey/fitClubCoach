518da29b0685b20168cfe7c4dcb1f3a0e3c7c63f
Dicky Budianto <dickybudianto@usahakreatif.co.id>
2020-07-07 08:44:10 +0700

    update back button checking

--- platforms/android/app/src/main/assets/www/assets/js/script.js
+++ platforms/android/app/src/main/assets/www/assets/js/script.js
@@ -1,5 +1,5 @@
 ﻿// var urlService = 'http://fitclubdev.zapto.org:8888/ronaldSengkey/fitClub/api/v1';
-var urlService = 'http://192.168.0.13:8888/ronaldSengkey/fitClub/api/v1';
+var urlService = 'http://localhost:8888/ronaldSengkey/fitClub/api/v1';
 var fieldTextInput = '<input type="text" class="form-control fieldText">';
 var fieldEmailInput = '<input type="email" class="form-control fieldEmail">';
 var fieldPswdInput = '<input type="password" class="form-control fieldPswd">';
